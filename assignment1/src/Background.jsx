22dd9a5f7c8caaa0302db98e5c35cf73c1fe5e8d
harinder24 <hss022002@gmail.com>
2023-05-26 08:37:13 -0700

    smok added

--- assignment1/src/Background.jsx
+++ assignment1/src/Background.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Background.css'
+import Smoke from './assets/Smoke'
 export default function Background() {
   return (
     
@@ -7,6 +8,7 @@ export default function Background() {
         <div className="space stars1"></div>
         <div className="space stars2"></div>
         <div className="space stars3"></div>
+        <Smoke/>
     </div>
   )
 }
---
+++ assignment1/src/assets/Smoke.jsx
@@ -0,0 +1,7 @@
+import './Smoke.css'
+import smokeImage from './smoke.png'
+export default function Smoke(){
+   
+    return <div className='smoke_images_wrapper'><img className='smoke_images' src={smokeImage}/></div>
+  };
+
