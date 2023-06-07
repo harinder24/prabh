b2553776e784096643ddb953617967bf5b788134
harinder24 <hss022002@gmail.com>
2023-06-07 02:56:02 -0700

    s

--- assignment1/src/main.jsx
+++ assignment1/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ExampleTwo from './ExampleTwo.jsx'
+import ExampleOne from './ExampleOne.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <App />
-
+    
 )
